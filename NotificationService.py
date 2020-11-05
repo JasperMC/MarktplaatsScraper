b00d7e20a5b9faf3c57113990e48aa3ae377336c
jaspercardol <37154053+jaspercardol@users.noreply.github.com>
2020-11-05 21:42:32 +0100

    Update NotificationService.py

--- NotificationService.py
+++ NotificationService.py
@@ -3,9 +3,7 @@ from pushover import init, Client
 class NotificationService:
     def __init__(self,api_token,user_token):
         self.api_token = api_token
-        "a5r3e32d6ghuqyq28u7m4s5cajdsjf"
         self.user_token = user_token
-        "uuc8mmii14jezy2uzpcwj2nc9ru4mi"
         self.device = ""
         init(self.api_token)
 
