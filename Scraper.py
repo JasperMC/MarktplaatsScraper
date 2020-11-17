339b01b4d847761cd82429a0d6d2e33a9bb11ca3
jaspercardol <37154053+jaspercardol@users.noreply.github.com>
2020-11-17 17:13:56 +0100

    Update Scraper.py

--- Scraper.py
+++ Scraper.py
@@ -20,10 +20,10 @@ class Scraper:
         driver.get(url)
         listings = {}
         for element in driver.find_elements_by_class_name("mp-Listing--list-item"):
-            if "mp-Listing--cas" in element.get_attribute('class'): # Filters out ads.
-                break;
-            if element.find_element_by_class_name('mp-Listing-seller-link'):
-                break;
+            #if "mp-Listing--cas" in element.get_attribute('class'): # Filters out ads.
+            #    break;
+            #if element.find_element_by_class_name('mp-Listing-seller-link'):
+            #    break;
             listing = {}
             listing['title'] = element.find_element_by_css_selector('h3.mp-Listing-title').text
             listing['description'] = element.find_element_by_css_selector('p.mp-Listing-description').text
